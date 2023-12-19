6fa7983351c3c7e5872f9ded05c81b98b9e83917
Njål Vadla <111281144+njalv@users.noreply.github.com>
2023-12-19 12:58:02 +0100

    Update chat-api-entry.ts

--- src/features/chat/chat-services/chat-api-entry.ts
+++ src/features/chat/chat-services/chat-api-entry.ts
@@ -1,18 +1,19 @@
 import { ChatAPIData } from "./chat-api-data";
-import { ChatAPISimple } from "./chat-api-simple";
 import { ChatAPIEnterprise } from "./chat-api-enterprise";
+import { ChatAPISimple } from "./chat-api-simple";
 import { PromptGPTProps } from "./models";
 
 export const chatAPIEntry = async (props: PromptGPTProps) => {
   if (props.chatType === "simple") {
     return await ChatAPISimple(props);
   } else if (props.chatType === "data") {
-    return await ChatAPIData(props);
+    return await ChatAPIData({ ...props, systemPrompt: props.systemPrompt, contextPrompt: props.contextPrompt });
   } else if (props.chatType === "enterprise") {
     return await ChatAPIEnterprise(props);
   } else if (props.chatType === "mssql") {
     return await ChatAPIData(props);
-  } else {
+  } 
+  else {
     return await ChatAPISimple(props);
   }
 };
